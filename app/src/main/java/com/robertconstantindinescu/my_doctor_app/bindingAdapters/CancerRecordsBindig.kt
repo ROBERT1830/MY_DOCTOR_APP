085ea67f8d54233820abe1e30ca84d079b133f36
ROBERT1830 <robert@beatingbrain.com>
2021-11-16 23:00:39 +0100

    hemos realizado la obtención de los datos de room en el recyclerView del fragment CancerRecordsFragment. Mostramos los datos y en función de si el resultado es maligno o benigno el color sale resaltado en rojo o verde. Además hacemos qeu se genere la fecha automáticamente para que el usuario sepa cuadno se hizo el pre-diagnóstico.

---
+++ app/src/main/java/com/robertconstantindinescu/my_doctor_app/bindingAdapters/CancerRecordsBindig.kt
@@ -0,0 +1,40 @@
+package com.robertconstantindinescu.my_doctor_app.bindingAdapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.robertconstantindinescu.my_doctor_app.adapters.CancerRecordsAdapter
+import com.robertconstantindinescu.my_doctor_app.models.data.database.entities.CancerDataEntity
+
+class CancerRecordsBindig {
+    companion object{
+
+        @BindingAdapter("viewVisibility", "setData", requireAll = false)
+        @JvmStatic
+        fun setDataAndViewVisibility(
+            view: View,
+            canceDataEntity: List<CancerDataEntity>?,
+            mapAdapter: CancerRecordsAdapter?
+        ){
+            if (canceDataEntity.isNullOrEmpty()){
+                when(view){
+                    is ImageView -> {view.visibility = View.VISIBLE}
+                    is TextView -> {view.visibility = View.VISIBLE}
+                    is RecyclerView -> {view.visibility = View.INVISIBLE}
+                }
+
+            }else{
+                when(view){
+                    is ImageView -> {view.visibility = View.INVISIBLE}
+                    is TextView -> {view.visibility = View.INVISIBLE}
+                    is RecyclerView -> {
+                        view.visibility = View.VISIBLE
+                        mapAdapter?.setData(canceDataEntity)
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/robertconstantindinescu/my_doctor_app/ui/fragments/CancerRecordsFragment.kt
+++ app/src/main/java/com/robertconstantindinescu/my_doctor_app/ui/fragments/CancerRecordsFragment.kt
@@ -1,22 +1,38 @@
 package com.robertconstantindinescu.my_doctor_app.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.robertconstantindinescu.my_doctor_app.R
+import com.robertconstantindinescu.my_doctor_app.adapters.CancerRecordsAdapter
 import com.robertconstantindinescu.my_doctor_app.databinding.FragmentCancerRecordsBinding
+import com.robertconstantindinescu.my_doctor_app.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class CancerRecordsFragment : Fragment() {
 //    // TODO: Rename and change types of parameters
 //    private var param1: String? = null
 //    private var param2: String? = null
 
-    private lateinit var mBinding: FragmentCancerRecordsBinding
+    //private lateinit var mBinding: FragmentCancerRecordsBinding
 
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private val mAdapter: CancerRecordsAdapter by lazy {
+        CancerRecordsAdapter(
+            requireActivity(),
+            mainViewModel
+        )
+    }
+
+    private var _binding: FragmentCancerRecordsBinding? = null
+    private val binding get () = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,33 +47,70 @@ class CancerRecordsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        mBinding = FragmentCancerRecordsBinding.inflate(inflater, container, false)
-        mBinding.newDetectionFab.setOnClickListener {
+        //mBinding = FragmentCancerRecordsBinding.inflate(inflater, container, false)
+
+        _binding = FragmentCancerRecordsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        binding.mAdapter = mAdapter
+
+        setHasOptionsMenu(true)
+        setUpRecyclerView(binding.recyclerViewCancerData)
+
+        binding.newDetectionFab.setOnClickListener {
             //navegamos al detector activity
             findNavController().navigate(R.id.action_cancerRecordsFragment_to_detectorActivity)
         }
 
-        return mBinding.root
+        return binding.root
+    }
+
+    private fun setUpRecyclerView(recyclerViewCancerData: RecyclerView) {
+        recyclerViewCancerData.adapter = mAdapter
+        recyclerViewCancerData.layoutManager = LinearLayoutManager(requireContext())
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserCancerDataFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CancerRecordsFragment().apply {
-                arguments = Bundle().apply {
-
-                }
-            }
+        inflater.inflate(R.menu.cancer_records_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.delete_all_cancer_records){
+            mainViewModel.deleteAllCancerRecords()
+            showSnackbar()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showSnackbar(){
+        Snackbar.make(
+            binding.root,
+            "All recipes removed",
+            Snackbar.LENGTH_SHORT
+        ).setAction("Ok"){}.show()
+    }
+
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment UserCancerDataFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            CancerRecordsFragment().apply {
+//                arguments = Bundle().apply {
+//
+//                }
+//            }
+//    }
 }
