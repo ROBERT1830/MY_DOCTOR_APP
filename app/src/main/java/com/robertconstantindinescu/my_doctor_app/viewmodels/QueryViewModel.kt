eff631c8d7c7bd260532075d73a526efb019108f
ROBERT1830 <robert@beatingbrain.com>
2021-11-19 08:29:35 +0100

    1-Hemos creado la clase QueryViewModel.kt que se encargará de montar la petición con los parametros de busqueda y sus valores en la funcion applyRadiationQuery.

---
+++ app/src/main/java/com/robertconstantindinescu/my_doctor_app/viewmodels/QueryViewModel.kt
@@ -0,0 +1,28 @@
+package com.robertconstantindinescu.my_doctor_app.viewmodels
+
+import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.AndroidViewModel
+import com.robertconstantindinescu.my_doctor_app.ui.fragments.RadiationFragment.Companion.latitude
+import com.robertconstantindinescu.my_doctor_app.ui.fragments.RadiationFragment.Companion.longitude
+import com.robertconstantindinescu.my_doctor_app.utils.Constants.Companion.API_KEY
+import com.robertconstantindinescu.my_doctor_app.utils.Constants.Companion.QUERY_API
+import com.robertconstantindinescu.my_doctor_app.utils.Constants.Companion.QUERY_LATITUDE
+import com.robertconstantindinescu.my_doctor_app.utils.Constants.Companion.QUERY_LONGITUDE
+
+class QueryViewModel @ViewModelInject constructor(
+    application: Application
+): AndroidViewModel(application){
+
+    fun applyRadiationQuery() : HashMap<String, String>{
+
+        val query: HashMap<String, String> = HashMap()
+        query[QUERY_LATITUDE] = latitude.toString()
+        query[QUERY_LONGITUDE] = longitude.toString()
+        query[QUERY_API] = API_KEY
+
+        return query
+
+    }
+
+}
