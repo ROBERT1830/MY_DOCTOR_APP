c5ca1c75b13705bb613a054bfa5292c21b8a3e2f
ROBERT1830 <robert@beatingbrain.com>
2021-12-01 12:26:21 +0100

    1-We have done the navigation between activities: LoginActivity.kt, SignUpChooseUsrActivity.kt, DoctorSignUpActivity.kt and PatientSignUpActivity.kt

--- app/src/main/java/com/robertconstantindinescu/my_doctor_app/ui/LoginActivity.kt
+++ app/src/main/java/com/robertconstantindinescu/my_doctor_app/ui/LoginActivity.kt
@@ -1,12 +1,44 @@
 package com.robertconstantindinescu.my_doctor_app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.robertconstantindinescu.my_doctor_app.R
+import com.robertconstantindinescu.my_doctor_app.databinding.ActivityLoginBinding
+import com.robertconstantindinescu.my_doctor_app.utils.LoadingDialog
 
 class LoginActivity : AppCompatActivity() {
+    //binding
+    private lateinit var mBinding: ActivityLoginBinding
+    private lateinit var loadingDialog: LoadingDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        //get bolean from intetn
+        val bundle :Bundle ?=intent.extras
+        if (bundle!=null){
+            val isDoctor = bundle.getBoolean("isDoctor") // 1
+
+            Log.i("isDoctor", isDoctor.toString())
+
+        }
+
+
+        loadingDialog = LoadingDialog(this)
+
+        with(mBinding) {
+            //llamas a una función con que tiene como parametro una lambda.
+            //if you put this, the context will be the mBinding (not what we want)
+            btnSignUp.setOnClickListener {
+                startActivity(Intent(this@LoginActivity,
+                    SignUpChooseUsrActivity::class.java))
+
+            }
+        }
     }
 }
--- app/src/main/java/com/robertconstantindinescu/my_doctor_app/ui/PatientSignUpActivity.kt
+++ app/src/main/java/com/robertconstantindinescu/my_doctor_app/ui/PatientSignUpActivity.kt
@@ -1,12 +1,28 @@
 package com.robertconstantindinescu.my_doctor_app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.robertconstantindinescu.my_doctor_app.R
+import com.robertconstantindinescu.my_doctor_app.databinding.ActivityPatientSignUpBinding
 
 class PatientSignUpActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityPatientSignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_patient_sign_up)
+        mBinding = ActivityPatientSignUpBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.txtLogin.setOnClickListener {
+            val intent =  Intent(this, LoginActivity::class.java)
+            intent.putExtra("isDoctor", false)
+            startActivity(intent)
+        }
+        mBinding.btnSignUp.setOnClickListener {
+            val intent =  Intent(this, LoginActivity::class.java)
+            intent.putExtra("isDoctor", false)
+            startActivity(intent)
+        }
     }
 }
