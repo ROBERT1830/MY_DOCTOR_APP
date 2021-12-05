fa66892b30064f652a94b2cfadc1993020523711
ROBERT1830 <robert@beatingbrain.com>
2021-12-05 21:29:52 +0100

    1-start activity in the onDirection method 2-Designed the activity_direction.xml and bottom_sheet_layout.xml. 3- added the adapter for the bottomsheet recycler.

---
+++ app/src/main/java/com/robertconstantindinescu/my_doctor_app/models/onlineData/directionPlaceModel/DirectionDurationModel.kt
@@ -0,0 +1,11 @@
+package com.robertconstantindinescu.my_doctor_app.models.googlePlaceModel.directionPlaceModel
+
+import com.squareup.moshi.Json
+
+data class DirectionDurationModel(
+    @field:Json(name = "text")
+    var text: String? = null,
+
+    @field:Json(name = "value")
+    var value: Int? = null
+)
---
+++ app/src/main/java/com/robertconstantindinescu/my_doctor_app/models/onlineData/directionPlaceModel/DirectionLegModel.kt
@@ -0,0 +1,34 @@
+package com.robertconstantindinescu.my_doctor_app.models.googlePlaceModel.directionPlaceModel
+
+
+import com.squareup.moshi.Json
+
+data class DirectionLegModel(
+    @field:Json(name = "distance")
+
+    val distance: DirectionDistanceModel? = null,
+
+    @field:Json(name = "duration")
+
+    val duration: DirectionDurationModel? = null,
+
+    @field:Json(name = "end_address")
+
+    val endAddress: String? = null,
+
+    @field:Json(name = "end_location")
+
+    val endLocation: EndLocationModel? = null,
+
+    @field:Json(name = "start_address")
+
+    val startAddress: String? = null,
+
+    @field:Json(name = "start_location")
+
+    val startLocation: StartLocationModel? = null,
+
+    @field:Json(name = "steps")
+
+    val steps: List<DirectionStepModel>? = null
+)
